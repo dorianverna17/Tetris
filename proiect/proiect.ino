7394edb8fd7f54cb393d3ccd438420303f2f658a
Dorian <dorianverna7@gmail.com>
2022-05-15 10:53:42 +0300

    Finished almost all implementation

--- proiect/proiect.ino
+++ proiect/proiect.ino
@@ -2,6 +2,9 @@
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
+/* for LCD */
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
 // Define hardware type, size, and output pins:
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
@@ -15,6 +18,14 @@ MD_MAX72XX myDisplay = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_
 
 /* other useful pins */
 #define POTENTIOMETER A0
+#define MOVE_LEFT 44
+#define MOVE_RIGHT 46
+#define ROTATE_LEFT 48
+#define ROTATE_RIGHT 50
+#define START_STOP 52
+
+/* lcd display */
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 /*
  * declare the matrix that represents the tetris game board
@@ -30,6 +41,8 @@ typedef struct moving_piece_struct {
   int size_piece;
   int x[2 * MAX_PIECE_SIZE];
   int y[2 * MAX_PIECE_SIZE];
+  /* the length of the piece */
+  int horizontal;
 } moving_piece;
 
 /* auxiliary for the piece generation */
@@ -39,9 +52,29 @@ int piece[ROWS][ROWS];
 int stopped;
 int first_it;
 int speed_pieces;
+int difficulty_speed;
 
 /* piece that is currently moving */
 moving_piece piece_mov;
+
+/* button state variables */
+int left_move;
+int right_move;
+int left_rotate;
+int right_rotate;
+int start_stop;
+/* used for play/pause */
+int continue_button;
+
+/* the score of a game */
+int score;
+/* score that needs to e achieved in order to win */
+int win_score;
+
+/* 0 - nothing, 1 - win, 2 - lose */
+int win_lose;
+
+int game_started;
 
 void generateRandomPiece();
 void displayMatrix();
@@ -64,7 +97,7 @@ void setup() {
   }
 
   /* set a random seed */
-  randomSeed(millis());
+  randomSeed(analogRead(POTENTIOMETER) + random(0, 1024));
 
   /* begin serial for debugging */
   Serial.begin(9600);
@@ -76,25 +109,131 @@ void setup() {
   stopped = 1;
   /* tells if it is the first iteration */
   first_it = 1;
+
+  /* game not started yet */
+  game_started = 0;
+
+  speed_pieces = analogRead(POTENTIOMETER);
+
+  /* begin lcd */
+  lcd.init();
+  lcd.init();
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello! Select");
+  lcd.setCursor(0, 1);
+  lcd.print("your difficulty");
 }
 
 void loop() {
-  if (stopped == 1 || first_it == 1) {
-    generateRandomPiece();
-    putPieceInMatrix();
-    first_it = 0;
-    stopped = 0;
+  if (game_started == 0) {
+    int potentiometer_value = analogRead(POTENTIOMETER);
+    
+    if (abs(potentiometer_value - speed_pieces) > 10) {
+      lcd.clear();
+      lcd.setCursor(6, 0);
+      if (potentiometer_value <= 333) {
+        lcd.print("EASY");
+        difficulty_speed = 800;
+        win_score = 3;
+      } else if (potentiometer_value > 333 && potentiometer_value <= 666) {
+        lcd.print("MEDIUM");
+        difficulty_speed = 400;
+        win_score = 5;
+      } else {
+        lcd.print("HARD");
+        difficulty_speed = 100;
+        win_score = 7;
+      }
+      lcd.setCursor(2, 1);
+      lcd.print("Press Start"); 
+    }
+
+    start_stop = digitalRead(START_STOP);
+    if (start_stop == 1) {
+      game_started = 1;
+      score = 0;
+      speed_pieces = difficulty_speed;
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Score:");
+      lcd.setCursor(8, 0);
+      lcd.print(score);
+
+      /* didn't lost or won yet */
+      win_lose = 0;
+    }
+  } else {
+    start_stop = digitalRead(START_STOP);
+    if (start_stop == 1) {
+      continue_button = 0;
+      delay(100);
+      while (continue_button == 0) {
+        lcd.setCursor(0, 1);
+        lcd.print("Game Paused");
+        continue_button = digitalRead(START_STOP);
+      }
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Score:");
+    }
+
+    lcd.setCursor(8, 0);
+    lcd.print(score);
+
+    lcd.setCursor(10, 0);
+    lcd.print("/");
+
+    lcd.setCursor(12, 0);
+    lcd.print(win_score);
+
+    if (win_score == score) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Congrats!");
+      lcd.setCursor(0, 1);
+      lcd.print("You Won!");
+      win_score = 1;
+      while (1);
+    }
+
+    if (win_score == 2) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Bad Luck...");
+      lcd.setCursor(0, 1);
+      lcd.print("You Lost:(");
+      while (1);
+    }
+    
+    if (stopped == 1 || first_it == 1) {
+      generateRandomPiece();
+      putPieceInMatrix();
+      first_it = 0;
+      stopped = 0;
+    }
+
+    eraseRows();
+
+    displayMatrix();
+
+    /* read the state of the move left button */
+    left_move = digitalRead(MOVE_LEFT);
+    /* read the state of the move right button */
+    right_move = digitalRead(MOVE_RIGHT);
+    // TODO -> matrice de incadrare
+    /* read the state of the rotate left button */
+    left_rotate = digitalRead(ROTATE_LEFT);
+    /* read the state of the rotate right button */
+    right_rotate = digitalRead(ROTATE_RIGHT);
+  
+    //  displayPiece();
+    movePieces();
+    delay(speed_pieces);
+
+    //  printMatrixSerial();
   }
-
-  displayMatrix();
-  displayPiece();
-  movePieces();
-  delay(speed_pieces);
-
-//  printMatrixSerial();
-
-  speed_pieces = analogRead(POTENTIOMETER);
-//  Serial.println(speed_pieces);
 }
 
 int checkIntersectionOfPiece() {
@@ -106,7 +245,7 @@ int checkIntersectionOfPiece() {
     y = piece_mov.y[i];
     if (y == COLUMNS - 1)
       return true;
-    else if (board[x][y + 1] == 1)
+    else if (y >= 0 && y < COLUMNS - 1 && board[x][y + 1] == 1)
       return true;
   }
   return false;
@@ -125,8 +264,108 @@ void stopPiece() {
   stopped = 1;
 }
 
+int checkLeftMove() {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      if (board[i][j] == 2) {
+        if (i == 0)
+          return false;
+        if (board[i - 1][j] == 1)
+          return false;
+      }
+    }
+  }
+  return true;
+}
+
+int MovePieceLeft() {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      if (board[i][j] == 2) {
+        board[i - 1][j] = board[i][j];
+        board[i][j] = 0;
+      }
+    }
+  }
+  for (int i = 0; i < piece_mov.size_piece; i++) {
+    piece_mov.x[i]--;
+  }
+  return true;
+}
+
+int checkRightMove() {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      if (board[i][j] == 2) {
+        if (i == ROWS - 1)
+          return false;
+        if (board[i + 1][j] == 1)
+          return false;
+      }
+    }
+  }
+  return true;
+}
+
+int MovePieceRight() {
+  for (int i = ROWS - 1; i >= 0; i--) {
+    for (int j = 0; j < COLUMNS; j++) {
+      if (board[i][j] == 2) {
+        board[i + 1][j] = board[i][j];
+        board[i][j] = 0;
+      }
+    }
+  }
+  for (int i = 0; i < piece_mov.size_piece; i++) {
+    piece_mov.x[i]++;
+  }
+  return true;
+}
+
+/* function that erases the rows that are full */
+void eraseRows() {
+  int check_row;
+  int i = COLUMNS - 1;
+
+  while (i >= 0) {
+    check_row = 1;
+    for (int j = 0; j < ROWS; j++) {
+      if (board[j][i] != 1) {
+        check_row = 0;
+        break;
+      }
+    }
+    if (check_row == 1) {
+      score += 1;
+      for (int j = 0; j < ROWS; j++) {
+        board[j][i] = 0;
+      }
+      for (int j = 0; j < ROWS; j++) {
+        for (int k = i; k >= 1; k--) {
+          board[j][k] = board[j][k - 1];
+        }
+      }
+      for (int j = 0; j < ROWS; j++) {
+        board[j][0] = 0;
+      }
+      i++;
+    }
+    i--;
+  }
+}
+
 /* In this function I move the pieces */
 void movePieces() {
+  /* check if we can move left */
+  if (left_move == 1) {
+    if (checkLeftMove()) {
+      MovePieceLeft();
+    }
+  } else if (right_move == 1) {
+    if (checkRightMove()) {
+      MovePieceRight();
+    }
+  }
   if (checkIntersectionOfPiece()) {
     stopPiece();
   }
@@ -140,9 +379,9 @@ void movePieces() {
   }
   for (int i = 0; i < piece_mov.size_piece; i++) {
     piece_mov.y[i]++;
-  }
-  for (int i = 0; i < ROWS; i++) {
-    board[i][0] = 0; 
+    if (piece_mov.y[i] == 0) {
+      board[piece_mov.x[i]][piece_mov.y[i]] = 2;
+    }
   }
 }
 
@@ -166,10 +405,8 @@ void displayMatrix() {
 /* function to check if the game has been lost */
 int checkIfLost() {
   for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < ROWS; j++) {
-      if (board[i][j] != 0 && piece[i][j] == 2)
-        return true;
-    }
+    if (board[i][0] == 1)
+      return true;
   }
   return false;
 }
@@ -181,21 +418,14 @@ void putPieceInMatrix() {
   int iterator = 0;
 
   if (checkIfLost()) {
-    Serial.println("You lost!");
+//    Serial.println("You lost!");
+    win_score = 2;
     while (1);
   }
-  
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < ROWS; j++) {
-      if (piece[i][j] == 2) {
-        board[i][j] = piece[i][j];
-        piece_mov.x[iterator] = i;
-        piece_mov.y[iterator] = j;
-        iterator++;
-      }
-    }
+
+  for (int i = 0; i < piece_mov.size_piece; i++) {
+    piece_mov.y[i] -= piece_mov.horizontal;
   }
-  piece_mov.size_piece = iterator;
 }
 
 /*
@@ -208,17 +438,14 @@ void generateRandomPiece() {
   int vertical;
   int horizontal;
   int intersection_point;
- 
-  /* initialize the matrix with zeros */
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < ROWS; j++) {
-      piece[i][j] = 0;
-    }
-  }
+  /* this is for constructing piece_mov */
+  int iterator = 0;
 
   /* starting_point */
   starting_point = random(0, ROWS);
-  piece[starting_point][0] = 2;
+  piece_mov.x[iterator] = starting_point;
+  piece_mov.y[iterator] = 0;
+  iterator++;
   
   vertical = random(2, MAX_PIECE_SIZE);
   if (vertical == 0)
@@ -233,12 +460,22 @@ void generateRandomPiece() {
 
   /* iterate over the next columns */
   for (int i = 0; i < horizontal; i++) {
-      piece[starting_point][i] = 2;
+      if (i != 0) {
+        piece_mov.x[iterator] = starting_point;
+        piece_mov.y[iterator] = i;
+        iterator++;
+      }
       if (intersection_point == i) {
         for (int j = 0; j < vertical; j++)
-          piece[starting_point + j][i] = 2;
+          if (j != 0) {
+            piece_mov.x[iterator] = starting_point + j;
+            piece_mov.y[iterator] = i;
+            iterator++;
+          }
       }
   }
+  piece_mov.size_piece = iterator;
+  piece_mov.horizontal = horizontal;
 }
 
 void printMatrixSerial() {
